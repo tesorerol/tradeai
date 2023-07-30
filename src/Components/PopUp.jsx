75a8c3c35a919ce4f78b28eb35ee168fa7354085
Luis Tesorero <luistesorero5@gmail.com>
2023-07-30 15:40:54 -0500

    changePopup

--- src/Components/PopUp.jsx
+++ src/Components/PopUp.jsx
@@ -7,7 +7,7 @@ const PopUp = ({togglePopup}) => {
     <div className='modal-header modal-popup'>
         <div className='container-popup aparecer'>
             <h2>Fomc Pool update</h2>
-            <p>all users claim sane time Monday morning EST</p>
+            <p>All users claim at the same time Monday am EST.</p>
             {/*<Link to="/earn-strategies/0x6E3138381633830dB1b29e30C912a2CC62d35B04" className='btn2' onClick={togglePopup}>Go Now</Link>*/}
             <AiOutlineClose className='close-icon' onClick={togglePopup} size={"25px"} color={"#FAFAFA"}/>
         </div>
