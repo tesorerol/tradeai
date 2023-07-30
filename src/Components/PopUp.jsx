5a934bdee01948a24dfa974d5bb5833e97b85eaa
Luis Tesorero <luistesorero5@gmail.com>
2023-07-30 11:33:13 -0500

    addPopup

--- src/Components/PopUp.jsx
+++ src/Components/PopUp.jsx
@@ -6,8 +6,9 @@ const PopUp = ({togglePopup}) => {
   return (
     <div className='modal-header modal-popup'>
         <div className='container-popup aparecer'>
-            <h2>New FOMC Degen Pool! Save the Whales</h2>
-            <Link to="/earn-strategies/0x6E3138381633830dB1b29e30C912a2CC62d35B04" className='btn2' onClick={togglePopup}>Go Now</Link>
+            <h2>Fomc Pool update</h2>
+            <p>all users claim sane time Monday morning EST</p>
+            {/*<Link to="/earn-strategies/0x6E3138381633830dB1b29e30C912a2CC62d35B04" className='btn2' onClick={togglePopup}>Go Now</Link>*/}
             <AiOutlineClose className='close-icon' onClick={togglePopup} size={"25px"} color={"#FAFAFA"}/>
         </div>
       
