e6fbe4be07667c56bbedc7077cab03daef4a5899
Luis Tesorero <luistesorero5@gmail.com>
2023-07-31 21:47:16 -0500

    change

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ document.addEventListener("keydown", function(e) {
               <Header toggleModal={toggleModal} />
               <div className='container-routes'>
                 <Routes>
-                  <Route path="/" element={<Navigate to="/earn-strategies/0x9b8a82B85034df40EfE34c4087c36D41fc559914"/>} />
+                  <Route path="/" element={<Navigate to="/earn-strategies/0x1242b3c2e7926750E2F3a0EB6aEBd4D865C3aCb8"/>} />
                   <Route path="/earn-strategies/:address" element={<EarnDinamic />} />
                   <Route path="*" element={<NotFound/>} />
                 </Routes>
