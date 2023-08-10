9fe9252cafab74689fad308088f4e8ac1ca81ef5
edgarbp95 <edgarbp95@hotmail.com>
2023-08-10 12:54:03 -0300

    add template multidepo

--- src/pages/Earns/EarnDinamic.jsx
+++ src/pages/Earns/EarnDinamic.jsx
@@ -8,36 +8,37 @@ import Pools from '../../Data/contracts.json';
 import AbiPrivate from '../../artifacts/contracts/EarnPrivate.sol/EARNPRIVATE.json'
 import AbiPublic from '../../artifacts/contracts/Earn.sol/BeNFTEARN.json'
 import AbiPass from '../../artifacts/contracts/EarnPass.sol/BeNFTEARNPASS.json'
+import AbiV3 from '../../artifacts/Test.json';
+import EarnTemplatev2 from './EarnTemplate2'
 const EarnDinamic = () => {
 
     const {data} = useSelector((state)=> state.contract)
 
     const dataContract = data
     const {address} = useParams()
+
+    function setTypeAbi(abi){
+      switch (abi) {
+        case "pass":
+          return AbiPass;
+        case "public":
+          return AbiPublic;
+        case "v3":
+          return AbiV3;
+        default:
+          return AbiPrivate
+      }
+     }
     
-    // useEffect(()=>{
-    //   if(dataContract !== ""){
-    //     const pool =  dataContract.filter(pool=> pool.name == name)
-    //     setDataPool(pool)
-    //     return
-    //   }
-    // },[dataContract])
-
-    // useEffect(()=>{
-    //   if(dataContract !== ""){
-    //     const pool =  dataContract.filter(pool=> pool.name == name)
-    //     setDataPool(pool)
-    //     return
-    //   }
-    // },[name])
-
     const pool =  Pools.filter(pool=> pool.address == address)
 
   return (
     <>
       {/* {dataContract == "" ? <Loading/> :  */}
       <>
-        {pool == "" ? <NotFound/> : <EarnTemplate table={pool[0].table} Pair={pool[0].pair} Abi={pool[0].AbiType==="pass"?AbiPass:pool[0].AbiType==="public"?AbiPublic:AbiPrivate} AbiType={pool[0].AbiType} claim={pool[0].claim} desposit={pool[0].desposit} unique={pool[0].unique} type={pool[0].type} strategy={pool[0].strategy} EarnContract={pool[0].address} namePool={pool[0].namePool}  risk={pool[0].risk} token={pool[0].token} infinite={pool[0].infinite}/>}
+        {pool == "" ? <NotFound/> 
+        : pool[0].AbiType === "v3" ? <EarnTemplatev2 Abi={setTypeAbi(pool[0].AbiType)} AbiType={pool[0].AbiType} claim={pool[0].claim} desposit={pool[0].desposit} unique={pool[0].unique} type={pool[0].type} strategy={pool[0].strategy} EarnContract={pool[0].address} namePool={pool[0].namePool}  risk={pool[0].risk} token={pool[0].token}/>
+        : <EarnTemplate table={pool[0].table} Pair={pool[0].pair} Abi={setTypeAbi(pool[0].AbiType)} AbiType={pool[0].AbiType} claim={pool[0].claim} desposit={pool[0].desposit} unique={pool[0].unique} type={pool[0].type} strategy={pool[0].strategy} EarnContract={pool[0].address} namePool={pool[0].namePool}  risk={pool[0].risk} token={pool[0].token} infinite={pool[0].infinite}/>}
       </>
       {/* } */}
     </>
---
+++ src/pages/Earns/EarnTemplate2.jsx
@@ -0,0 +1,458 @@
+import React, { useContext, useEffect, useRef, useState } from 'react'
+import { FaUserAlt } from 'react-icons/fa'
+import Button2 from '../../Components/Button2'
+import usdt from '../../assets/icons/usdt.png'
+import { BsQuestionSquare } from 'react-icons/bs'
+import { ethers } from 'ethers'
+import { WalletContext } from '../../Providers/WallectConnect'
+import ERC20 from '../../artifacts/contracts/USDT.sol/TetherToken.json'
+import { Allowance, Approve, MModeTimer, formatMoney, formatMoney2 } from '../../Helpers'
+import Swal from 'sweetalert2'
+import Loader from '../../Components/Loading'
+import ProgressBar from '../../Components/ProgressBar'
+
+const EarnTemplatev2 = (props) => {
+  const [percentage, setPercentage] = useState(null)
+
+  const { EarnContract, namePool, strategy, risk, token, type, AbiType,Abi,claim,desposit,unique } = props
+  const { Provider, address } = useContext(WalletContext)
+  let USDT = '0x55d398326f99059fF775485246999027B3197955'
+  const [Recolect, setRecolect] = useState('0')
+  const [UserInfo, setUserInfo] = useState([])
+  const [Aprove, setAprove] = useState(true)
+  const [Amount, setAmount] = useState('0')
+  const [getMaxUser, setgetMaxUser] = useState('0')
+  const [AmountAprove, setAmountAprove] = useState('0')
+  const [Tx, setTx] = useState(false)
+  const [UsdtBalance, setUsdtBalance] = useState('0')
+  const [UsdtBalanceContract, setUsdtBalanceContract] = useState('0')
+  const [FinishTime, setFinishTime] = useState('0')
+  const [mintAmount, setmintAmount] = useState('0')
+  const [maxAmount, setmaxAmount] = useState('0')
+  const [limitAmount, setlimitAmount] = useState('0')
+  const [percentOfContract, setpercentOfContract] = useState('0')
+  const [Loading, setLoading] = useState(false)
+  let contract = new ethers.Contract(EarnContract, Abi.abi, Provider)
+  let intervalid
+  const bar = useRef()
+  useEffect(() => {
+    TotalRecolect()
+    MaxAmount()
+    MinAmount()
+    PercentOfContract()
+    GetUsdtBalanceContracts()
+    if (address) {
+      GetUserInfo()
+      Intermitente()
+      GetBusdBalance()
+      if(AbiType == 'pass'){
+        getMaxAmount()
+        }
+      Allowance(Provider, address, EarnContract, USDT).then((r) => {
+        if (parseInt(ethers.utils.formatEther(r)) >= 1000000) {
+          setAmountAprove(ethers.utils.formatEther(r))
+          setAprove(false)
+        } else {
+          setAprove(true)
+        }
+      })
+    }
+    return () => {
+      clearInterval(intervalid)
+      setpercentOfContract("0");
+    }
+  }, [address, EarnContract])
+
+  function getMaxAmount() {
+    contract.getMaxPerUser(address).then((r) => {
+      setgetMaxUser(ethers.utils.formatEther(r))
+    })
+  }
+  function TotalRecolect() {
+    contract.recolect().then((total) => {
+      contract.limit().then((limit) => {
+        setlimitAmount(ethers.utils.formatEther(limit))
+        setRecolect(ethers.utils.formatEther(total))
+        // bar.current.style.width = `${
+        //   (parseFloat(ethers.utils.formatEther(total)) /
+        //     parseFloat(ethers.utils.formatEther(limit))) *
+        //   100
+        // }%`
+
+        const percent = (parseFloat(Math.floor((ethers.utils.formatEther(total)) / parseFloat(ethers.utils.formatEther(limit)) * 100)))
+        setPercentage(percent)
+        
+        // bar.current.style.width = (parseFloat(ethers.utils.formatEther(total)) / parseFloat(ethers.utils.formatEther(limit))) * 100
+      })
+    })
+  }
+
+  function MinAmount() {
+    contract.MinAmount().then((r) => setmintAmount(ethers.utils.formatEther(r)))
+  }
+
+  function MinAmount() {
+    contract.MinAmount().then((r) => setmintAmount(ethers.utils.formatEther(r)))
+  }
+
+  function PercentOfContract() {
+    contract.PercentEarn().then((r) => setpercentOfContract(r.toString()))
+  }
+
+  function MaxAmount() {
+    contract.MaxAmount().then((r) => setmaxAmount(ethers.utils.formatEther(r)))
+  }
+
+  function GetUserInfo() {
+    let data =[];
+     contract.depositCount(address).then(async (count) => {
+         for (let index = 0; index < count.toString(); index++) {
+             await contract.balanceUser(address,index).then((r) => {
+                 data.push({
+                     Amount:ethers.utils.formatEther(r.Amount),
+                     Earn: ethers.utils.formatEther(r.Earn),
+                     Time:MModeTimer(r.finishTime.toString())
+                 })
+               })
+         }
+         setUserInfo(data)
+     })
+
+
+
+  }
+  function Intermitente() {
+    intervalid = setInterval(() => {
+      GetUserInfo()
+    }, 1000)
+  }
+
+  async function Deposit() {
+    let signer = Provider.getSigner()
+    setLoading(true)
+    let contract2 = new ethers.Contract(EarnContract, Abi.abi, signer)
+    let amunt = ethers.utils.parseEther(Amount)
+    contract2
+      .Deposit(amunt)
+      .then(async (r) => {
+        setTx(r.hash)
+        r.wait()
+          .then((r) => {
+            TotalRecolect()
+            GetUserInfo()
+            setLoading(false)
+            setAmount('0')
+            GetBusdBalance()
+            setTx(false)
+          })
+          .catch((e) => {
+            console.log(e)
+          })
+      })
+      .catch((e) => {
+        setLoading(false)
+        const errorMessage = e.error.data.message
+        Swal.fire({
+          icon: 'error',
+          title: errorMessage,
+        })
+      })
+  }
+
+  function Witdraw(depositID) {
+    let signer = Provider.getSigner()
+    setLoading(true)
+    let contract2 = new ethers.Contract(EarnContract, Abi.abi, signer)
+      contract2
+        .EarnDeposit(depositID)
+        .then((r) => {
+          GetBusdBalance();
+          setLoading(false)
+        })
+        .catch((e) => {
+          setLoading(false)
+          const errorMessage = e.error.data.message
+          Swal.fire({
+            icon: 'error',
+            title: errorMessage,
+          })
+        })
+  }
+
+  function GetBusdBalance() {
+    let signer = Provider.getSigner()
+    let contract2 = new ethers.Contract(USDT, ERC20.abi, signer)
+    contract2
+      .balanceOf(address)
+      .then((r) => setUsdtBalance(ethers.utils.formatEther(r)))
+  }
+
+  function GetUsdtBalanceContracts() {
+    let contract2 = new ethers.Contract(USDT, ERC20.abi, Provider)
+    contract2
+      .balanceOf(EarnContract)
+      .then((r) => setUsdtBalanceContract(ethers.utils.formatEther(r)))
+  }
+
+  return (
+    <div className="earn-strategies">
+      {Loading && <Loader />}
+      <div className='earn-strategies-ai'>
+        <div className='earn-strategies-ai-tittle'>
+            <p>{namePool}</p>
+            <h3>
+              {type} Yield: {unique?"Surprise ": `${percentOfContract} %` }
+            </h3>
+        </div>
+        <div className='earn-strategies-ai-info'>
+              <div className='info-left'>
+                <div className='info-left-1'>
+                  <ul>
+                    <li>
+                      <p>Strategy: <span>{strategy}</span></p>
+                    </li>
+                    <li>
+                      <div className='risk-token'>
+                        <p className='risk-token-token'>Token: <span>{token}</span></p>
+                        <div className="strategies-txt-risk">
+                          <p>
+                            Risk: <span>{risk}</span>
+                          </p>
+                          <div className="question-icon">
+                            <BsQuestionSquare size={'20px'} color="#245a78" />
+                            <p className="info-risk">
+                            Low leverage trading (2x to 5x){' '}
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </li>
+                  
+                    <li className='address-header'>
+                      <p>
+                      Address:{' '}
+                        <span>
+                          <a
+                            href={`https://bscscan.com/address/${EarnContract}`}
+                            target="_blank"
+                          >
+                            {EarnContract}
+                          </a>
+                        </span>
+                        </p>
+                    </li>
+                    <li>
+                      <p>
+                        SC Balance: {" "}
+                        <span>{formatMoney(UsdtBalanceContract, 'USDT', 2, '.', ',')}</span>
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+                
+              </div>
+              <div className='info-middle'>
+                <ProgressBar percentage={percentage} size={100} />
+              </div>
+              <div className='info-right'>
+                  <div className='info-right-1'>
+                    <ul>
+                      <li>
+                        <p>Pool size:</p>
+                        <p><span> {formatMoney(limitAmount, 'USDT', 2, '.', ',')}</span></p>
+                      </li>
+                      <li>
+                        <p>Current Deposit:</p>
+                        <p>{formatMoney(Recolect, 'USDT', 2, '.', ',')}</p>
+                      </li>
+                      
+                    </ul>
+                  </div>
+                  <div className='info-right-2'>
+                    <ul>
+                      <li>
+                        <p>Min entry:</p>
+                        <p>
+                          <span>{formatMoney(mintAmount, 'USDT', 2, '.', ',')}</span>
+                        </p>
+                      </li>
+                      <li>
+                        <p>Max entry:</p>
+                        <p>
+                          <span>
+                          {' '}
+                          {getMaxUser > 0
+                            ? formatMoney(getMaxUser, 'USDT', 2, '.', ',')
+                            : formatMoney(maxAmount, 'USDT', 2, '.', ',')}
+                          </span>
+                        </p>
+                      </li>
+                    </ul>
+                  </div>
+              </div>
+        </div>
+        <div className='earn-strategies-ai-details earn-strategies-ai-details-v2'>
+          <div className='earn-strategies-ai-details-deposit'>
+            <h2>Deposit</h2>
+            <div className="approve">
+                {
+                  !Aprove &&
+                  (
+                    <p className="amount-approved">
+                      Amount approved for use:{' '}
+                      <span>
+                        {formatMoney(AmountAprove, 'USDT', 2, '.', ',')}
+                      </span>
+                    </p>
+                  )
+                }
+                
+              </div>
+              <div className="details-staked">
+                <div className="details-staked-input">
+                  <div className="input-button">
+                    <div className="div-input-deposit">
+                      <input
+                        className="input-deposit"
+                        placeholder="0.0"
+                        type="number"
+                        value={Amount}
+                        onChange={(e) => {
+                          setAmount(e.target.value)
+                        }}
+                      />
+                      <div className="input-usdt">
+                        <img
+                          className="input-icon"
+                          src={usdt}
+                          alt="Logo de USDT"
+                        />
+                        <p>USDT</p>
+                      </div>
+                      <div className="balance-input">
+                        <p>
+                          Balance:{' '}
+                          <span>
+                            {formatMoney(UsdtBalance, '', 2, '.', ',')}
+                          </span>
+                        </p>
+                        <button
+                          className="max-input"
+                          onClick={() =>
+                            setAmount(
+                              formatMoney(UsdtBalance, '', 2, '.', ',').trim(),
+                            )
+                          }
+                        >
+                          MAX
+                        </button>
+                      </div>
+                    </div>
+                    {Aprove ? (
+                  <button className='button2' onClick={() => {
+                    setLoading(true)
+                    Approve(Provider, EarnContract, USDT)
+                      .then((r) => {
+                        Allowance(Provider, address, EarnContract, USDT).then(
+                          (r) => {
+                            if (
+                              parseInt(ethers.utils.formatEther(r)) >= 1000000
+                            ) {
+                              setAprove(false)
+                              Swal.fire({
+                                icon: 'success',
+                                title: 'Tokens Approve',
+                                showConfirmButton: true,
+                              })
+                            } else {
+                              setAprove(true)
+                              Swal.fire({
+                                icon: 'error',
+                                title: 'you need approve more than 0 or use max default',
+                                showConfirmButton: true,
+                              })
+                            }
+                            setLoading(false)
+                          },
+                        )
+                      })
+                      .catch((r) => setLoading(false))
+                  }}>
+                    <span className='button-content'>Approve</span>
+                  </button>
+                  
+                ) : (<button disabled={!desposit?true:false} className="button2" onClick={() => desposit?Deposit():console.log()}>
+                {desposit?"Deposit":"Deposit disabled"}
+                </button>
+                )}
+                  </div>
+                </div>
+              </div>
+            
+          </div>
+          <div className='earn-strategies-ai-details-details'>
+            <h2>Your position</h2>
+            {claim &&
+            UserInfo.map((r,index)=>
+          <div className="progress-section-details-staked">
+                <div className="table-staked2">
+                  <div className="table-row row1">
+                    <div>
+                      <h3>Your stake:</h3>
+                    </div>
+                    <p className="table-number">
+                      {formatMoney(r.Amount, 'USDT')}
+                    </p>
+                  </div>
+                  <div className="table-row row1">
+                    <div>
+                      <h3>Pending Earn:</h3>
+                    </div>
+                    <p className="table-number">
+                      
+                      {formatMoney(
+                        parseFloat(r.Earn)-parseFloat(r.Amount),
+                        'USDT',
+                      )}
+                    </p>
+                  </div>
+                  <div className="table-row ">
+                    <div>
+                      <h3>Total payout:</h3>
+                    </div>
+                    <p className="table-number">
+                      {unique
+                      ? "Surprise"
+                      :formatMoney(r.Earn, 'USDT')}
+                    </p>
+                  </div>
+                  <div className="table-row ">
+                    <div>
+                      <h3>Timer:</h3>
+                    </div>
+                    <p className="table-number table-timer" onClick={() => (r.Time ? console.log() : Witdraw(index))}>
+                      {r.Time ? (
+                        <span className='button-content'>{r.Time}</span>
+                      ) : (
+                        <span className='button-content button-claim'>
+                          <i className="fa-solid fa-lock mr-1"></i>
+                          {'Claim'}
+                        </span>
+                      )}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            )
+            }
+          </div>
+
+        </div>
+        
+
+      </div>
+    </div>
+  )
+}
+
+export default EarnTemplatev2
